558b639449b3bfe22d08db45826d01441c978a74
Dinei Ruiz <dinerisruiz@gmail.com>
2024-10-01 07:59:23 -0300

    Deasafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia meuNokia = new Nokia("1197083-9569","Nokia trends 2024","78695844220",64);
+Console.WriteLine($"Numero  do Nokia:{meuNokia.Numero}");
+meuNokia.Ligar();
+meuNokia.ReceberLigacao();
+meuNokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+
+Iphone meuIphone = new Iphone("0119709839569","Iphone 18","784515646556",128);
+Console.WriteLine($"Numero do Iphone {meuIphone.Numero}");
+meuIphone.Ligar();
+meuIphone.ReceberLigacao();
+meuIphone.InstalarAplicativo("Intagram");
